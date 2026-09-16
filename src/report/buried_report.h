901861cee9e4ee6ffb29cc600102ecd0704f50b3
ch0sen1pm <18987934743@163.com>
2026-09-16 23:46:56 +0800

    feat: add BuriedReport interface

---
+++ src/report/buried_report.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+#include "common/common_service.h"
+
+namespace spdlog {
+class logger;
+} // namespace spdlog
+
+namespace buried {
+struct BuriedData {
+    std::string title;
+    std::string data;
+    std::uint32_t priority;
+};
+
+class BuriedReportImpl;
+
+class BuriedReport {
+public:
+    BuriedReport(std::shared_ptr<spdlog::logger> logger,
+                 CommonService common_service,
+                 std::string work_path);
+
+    ~BuriedReport();
+    
+    void Start();
+
+    void InsertData(const BuriedData& data);
+
+private:
+    std::unique_ptr<BuriedReportImpl> impl_;
+};
+
+
+} // namespace buried
